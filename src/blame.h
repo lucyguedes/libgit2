3e35ff392f6d6dfb2eaeee6866457006a90a2abf
Ben Straub <bs@github.com>
2013-07-17 06:24:47 -0700

    Record expected line positions for linear commits

--- src/blame.h
+++ src/blame.h
@@ -3,6 +3,31 @@
 #include "vector.h"
 #include "diff.h"
 #include "array.h"
+#include "git2/oid.h"
+
+/* Experimental line-map */
+#define kmalloc git__malloc
+#define kcalloc git__calloc
+#define krealloc git__realloc
+#define kfree git__free
+#include "khash.h"
+__KHASH_TYPE(line, git_oid, uint16_t);
+typedef khash_t(line) blame_linemap;
+GIT_INLINE(khint_t) linemap_hash(git_oid oid)
+{
+	khint_t h;
+	memcpy(&h, &oid, sizeof(khint_t));
+	return h;
+}
+GIT_INLINE(bool) linemap_equal(git_oid a, git_oid b)
+{
+	return git_oid_equal(&a, &b);
+}
+#define GIT__USE_LINEMAP \
+	__KHASH_IMPL(line, static kh_inline, git_oid, uint16_t, 1, linemap_hash, linemap_equal)
+#define blame_linemap_alloc() kh_init(line)
+#define blame_linemap_free(h) kh_destroy(line,h), h = NULL
+
 typedef struct blame_hunk {
 	/* git_blame_hunk fields */
 	uint16_t lines_in_hunk;
@@ -11,6 +36,9 @@ typedef struct blame_hunk {
 	git_oid orig_commit_id;
 	const char *orig_path;
 	uint16_t orig_start_line_number;
+
+	/* Internal fields */
+	blame_linemap *linemap;
 } blame_hunk;
 
 struct git_blame {
@@ -27,6 +55,7 @@ struct git_blame {
 	git_array_t(size_t) line_index;
 
 	git_oid current_commit;
+	git_oid parent_commit;
 	size_t current_diff_line;
 	size_t current_blame_line;
 	blame_hunk *current_hunk;
